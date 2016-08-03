9e17c52722ea16b9c2632f30a9c76f7137649183
Philip Cressler <philipcressler@Philip-Cresslers-iMac.local>
2016-08-03 17:25:42 -0500

    initial setup

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,8 @@ var runSequence  = require('run-sequence');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var uglify       = require('gulp-uglify');
+var prompt       = require('gulp-prompt');
+var rsync        = require('gulp-rsync');
 
 // See https://github.com/austinpray/asset-builder
 var manifest = require('asset-builder')('./assets/manifest.json');
@@ -286,3 +288,55 @@ gulp.task('wiredep', function() {
 gulp.task('default', ['clean'], function() {
   gulp.start('build');
 });
+
+gulp.task('deploy', function() {
+    // Dirs and Files to sync
+    rsyncPaths = [path.dist, 'lang', 'lib', 'templates', './*.php', './style.css' ];
+
+    // default options for rsync
+    rsyncConf = {
+        progress: true,
+        incremental: true,
+        relative: true,
+        emptyDirectories: true,
+        recursive: true,
+        clean: true,
+        exclude: [],
+    };
+
+    // dev environment
+    if(argv.dev) {
+        rsyncConf.hostname = ''; //hostname
+        rsyncConf.username = ''; //ssh username
+        rsyncConf.destination = ''; // path to uploaded files
+    else if (argv.production) {
+
+        rsyncConf.hostname = ''; // hostname
+        rsyncConf.username = ''; // ssh username
+        rsyncConf.destination = ''; // path where uploaded files go
+                
+              
+    // Missing/Invalid Target  
+    } else {
+        throwError('deploy', gutil.colors.red('Missing or invalid target'));
+    }
+                        
+
+    // Use gulp-rsync to sync the files 
+    return gulp.src(rsyncPaths)
+    .pipe(gulpif(
+        argv.production, 
+        prompt.confirm({
+            message: 'Heads Up! Are you SURE you want to push to PRODUCTION?',
+            default: false
+        })
+    ))
+    .pipe(rsync(rsyncConf));
+});
+                
+function throwError(taskName, msg) {
+    throw new gutil.PluginError({
+        plugin: taskName,
+        message: msg
+    });
+}
